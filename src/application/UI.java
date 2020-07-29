553a3d333e22628400008fc2ee737588b35c3ae5
Felipe Lopes <felipe.lopes@somoldrj.local>
2020-07-29 11:02:04 -0300

    Queen correcao erro UI

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,7 +58,7 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: "+chessMatch.getTurn());
-		if (chessMatch.getcheckMate()) {
+		if (!chessMatch.getcheckMate()) {
 			System.out.println("Waiting player: "+chessMatch.getCurrentPlayer());
 			if (chessMatch.getcheck()) {
 				System.out.println("Check");
@@ -69,7 +69,7 @@ public class UI {
 			System.out.println("CHECKMATE!");
 			System.out.println("Winner: "+chessMatch.getCurrentPlayer());;
 		}
-		System.out.println("Waiting player: "+chessMatch.getCurrentPlayer());
+
 		if (chessMatch.getcheck()) {
 			System.out.println("CHECK!");
 		}
